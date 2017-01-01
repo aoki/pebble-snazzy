2a709c6b467d11b6e408de25ddcec85c0584f52b
aoki <ringohub@gmail.com>
2017-01-01 17:32:53 +0900

    Show date

--- src/c/main.c
+++ src/c/main.c
@@ -1,10 +1,10 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-
 static TextLayer *s_time_layer;
-
+static TextLayer *s_date_layer;
 static GFont s_time_font;
+
 
 static void update_time() {
   // Get a tm structure
@@ -13,13 +13,13 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
-
-  // Display this time on the TextLayer
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
   text_layer_set_text(s_time_layer, s_buffer);
+  
+  static char s_buffer_date[] = "Jan 01 Wed";
+  strftime(s_buffer_date, sizeof(s_buffer_date), "%b %d %a", tick_time);
+  text_layer_set_text(s_date_layer, s_buffer_date);
 }
-
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
@@ -32,21 +32,29 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_CODE_PRO_32));
+//   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SOURCE_CODE_PRO_REGULAR_40));
 
-  // Create the TextLayer with specific bounds
+  // Date
+  s_time_font = fonts_get_system_font(FONT_KEY_GOTHIC_24);
+  s_date_layer = text_layer_create(
+      GRect(0, 30, bounds.size.w, 24));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_font(s_date_layer, s_time_font);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+  
+  // Time
+  s_time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT);
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-//   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  
 }
 
 static void main_window_unload(Window *window) {
@@ -71,7 +79,6 @@ static void init() {
 }
 
 static void deinit() {
-  // Destroy Window
   window_destroy(s_main_window);
 }
 
